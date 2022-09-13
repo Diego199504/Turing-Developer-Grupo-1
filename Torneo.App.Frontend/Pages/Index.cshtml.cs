0620fa479ee77dbbedb124628d19db698284a704
Omar Ricardo Moreno <omar.moreno@mail.com>
2022-09-13 16:07:36 -0500

    arreglo errores de using

--- Torneo.App.Frontend/Pages/Index.cshtml.cs
+++ Torneo.App.Frontend/Pages/Index.cshtml.cs
@@ -1,20 +1,20 @@
 ﻿using Torneo.App.Persistencia;
-using Torneo.App.Dominio;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Torneo.App.Frontend.Pages.Partido;
 
-    public class IndexModel : PageModel
+public class IndexModel : PageModel
 {
-        private readonly IRepositorioPartido _repoPartido;
-        public IEnumerable<Partido> Partido {get;set;}
-        public IndexModel(IRepositorioPartido repoPartido)
-        {
-            _repoPartido = repoPartido;
-        }
+    private readonly IRepositorioPartido _repoPartido;
+    public IEnumerable<Dominio.Partido> Partido { get; set; }
 
-        public void OnGet()
-        {
-            Partido = _repoPartido.GetAllPartidos();
-            
-        }
+    public IndexModel(IRepositorioPartido repoPartido)
+    {
+        _repoPartido = repoPartido;
+    }
+
+    public void OnGet()
+    {
+        Partido = _repoPartido.GetAllPartidos();
+    }
 }
--- Torneo.App.Frontend/Pages/Partido/Details.cshtml.cs
+++ Torneo.App.Frontend/Pages/Partido/Details.cshtml.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Torneo.App.Dominio;
 using Torneo.App.Persistencia;
 
@@ -6,15 +8,16 @@ namespace Torneo.App.Frontend.Pages.Partido
     public class DetailsModel : PageModel
     {
         private readonly IRepositorioPartido _repoPartido;
-        public IEnumerable<Partido> partido {get;set;}
+        public Dominio.Partido partido { get; set; }
         public DetailsModel(IRepositorioPartido repoPartido)
         {
             _repoPartido = repoPartido;
         }
-            
+
         public IActionResult OnGet(int id)
         {
             partido = _repoPartido.GetPartido(id);
+
             if (partido == null)
             {
                 return NotFound();
