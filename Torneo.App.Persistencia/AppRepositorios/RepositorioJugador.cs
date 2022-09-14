f8fc616c1be03e7029091347c54eee9975c76ac1
Diego199504 <diego.giraldo03@usc.edu.co>
2022-09-14 12:12:35 -0500

    Creacion de Details Jugador

--- Torneo.App.Persistencia/AppRepositorios/RepositorioJugador.cs
+++ Torneo.App.Persistencia/AppRepositorios/RepositorioJugador.cs
@@ -2,41 +2,41 @@ using Microsoft.EntityFrameworkCore;
 using Torneo.App.Dominio;
 namespace Torneo.App.Persistencia
 {
- public class RepositorioJugador : IRepositorioJugador
- {
-    private readonly DataContext _dataContext= new DataContext(); 
-         public Jugadores AddJugador(Jugadores jugador, int idPosicion, int idEquipo)
+    public class RepositorioJugador : IRepositorioJugador
+    {
+        private readonly DataContext _dataContext = new DataContext();
+        public Jugadores AddJugador(Jugadores jugador, int idPosicion, int idEquipo)
         {
-          
-             var posicionEncontrado = _dataContext.Posicion.Find(idPosicion);
-             var equipoEncontrado = _dataContext.Equipos.Find(idEquipo);
-             jugador.Posicion = posicionEncontrado;
-             jugador.Equipo = equipoEncontrado;
-             var jugadorInsertado = _dataContext.Jugadores.Add(jugador);
-             _dataContext.SaveChanges();
-             return jugadorInsertado.Entity;
+
+            var posicionEncontrado = _dataContext.Posicion.Find(idPosicion);
+            var equipoEncontrado = _dataContext.Equipos.Find(idEquipo);
+            jugador.Posicion = posicionEncontrado;
+            jugador.Equipo = equipoEncontrado;
+            var jugadorInsertado = _dataContext.Jugadores.Add(jugador);
+            _dataContext.SaveChanges();
+            return jugadorInsertado.Entity;
         }
 
-         public IEnumerable<Jugadores> GetAllJugadores()
+        public IEnumerable<Jugadores> GetAllJugadores()
         {
-             var jugadores = _dataContext.Jugadores
-
-                .Include(e => e.Posicion)
-                .Include(e => e.Equipo)
-                .ToList();
+            var jugadores = _dataContext.Jugadores
+               .Include(e => e.Posicion)
+               .Include(e => e.Equipo)
+               .ToList();
             return jugadores;
-        } 
+        }
 
         public Jugadores GetJugador(int idJugador)
-         {
+        {
             var jugadorEncontrado = _dataContext.Jugadores
                 .Where(e => e.Id == idJugador)
                 .Include(e => e.Posicion)
                 .Include(e => e.Equipo)
-                .FirstOrDefault();
-            return jugadorEncontrado;    
+                //.FirstOrDefault();
+                .ToList();
+            return jugadorEncontrado;
 
-         }   
-        
+        }
+
     }
 }
