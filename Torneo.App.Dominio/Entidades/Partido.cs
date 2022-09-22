efd3110d50e68beea8ad621b3a4523499db1cca9
Diego199504 <diego.giraldo03@usc.edu.co>
2022-09-22 15:49:04 -0500

    Boton Edit Partido

--- Torneo.App.Dominio/Entidades/Partido.cs
+++ Torneo.App.Dominio/Entidades/Partido.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace Torneo.App.Dominio
@@ -7,9 +8,13 @@ namespace Torneo.App.Dominio
     public class Partido
     {
         public int Id { get; set; }
+
+        [Display(Name = "Programe la Hora del Partido")]
         public DateTime FechaHora { get; set; }
         //public Equipo Equipo {get; set; }
+        [Required(ErrorMessage = "Seleccionar un Equipo Visitante es Obligatorio")]
         public Equipo Visitante {get; set; }
+        [Required(ErrorMessage = "Seleccionar un Equipo Local es Obligatorio")]
         public Equipo Local {get; set; }
         public int MarcadorEquipoLocal {get; set; }
         public int MarcadorEquipoVisitante {get; set; }
--- Torneo.App.Frontend/Pages/Partidos/Edit.cshtml.cs
+++ Torneo.App.Frontend/Pages/Partidos/Edit.cshtml.cs
@@ -1,12 +1,52 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering; // Libreria para trabajar con selects 
+using Torneo.App.Dominio;
+using Torneo.App.Persistencia;
 
 namespace Torneo.App.Frontend.Pages.Partidos
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioPartido _repoPartido;
+        private readonly IRepositorioEquipo _repoEquipoLocal;
+        private readonly IRepositorioEquipo _repoEquipoVisitante;
+
+        public Partido partido { get; set; }
+        public SelectList equipoLOption{get; set;}
+        public SelectList equipoVOption{get; set;}
+
+        public int equipoLSelected { get; set; }
+        public int equipoVSelected { get; set; }
+
+        //Instanciar las propiedades
+         public EditModel(IRepositorioPartido repoPartido, IRepositorioEquipo repoEquipoLocal,IRepositorioEquipo repoEquipoVisitante)
         {
+            _repoPartido = repoPartido;
+            _repoEquipoLocal = repoEquipoLocal;
+            _repoEquipoVisitante = repoEquipoVisitante;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            partido = _repoPartido.GetPartido(id);
+            equipoLOption = new SelectList(_repoPartido.GetAllPartidos(), "Id", "Nombre");
+            equipoLSelected = partido.Local.Id;
+            equipoVOption = new SelectList(_repoPartido.GetAllPartidos(), "Id", "Nombre");
+            equipoVSelected = partido.Visitante.Id;
+            if (partido == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Page();
+            }
+        }
+        public IActionResult OnPost(Partido partido, int idEquipoLocal, int idEquipoVisitante)
+        {
+            _repoPartido.UpdatePartido(partido, idEquipoLocal, idEquipoVisitante);
+            return RedirectToPage("Index");
         }
     }
 }
